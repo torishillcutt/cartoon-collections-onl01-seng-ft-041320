f34c68a3e5d9fb466e567b5e6c02850c601c9527
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-16 22:46:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 def long_planeteer_calls(array)
   if array.any? {|element| element.length >= 4 }
     true
-  elsif array.all? {|element| element.length < 4}
+  elsif array.none? {|element| element.length >= 4}
     false
   end
 end
