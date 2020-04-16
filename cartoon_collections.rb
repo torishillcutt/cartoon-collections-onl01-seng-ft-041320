205dc018cf22e28fb2fc872d316281aa35f0ac82
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-16 23:02:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-   if array.any? {|element| element.length >= 4} = true
+   if array.any? {|element| element.length >= 4} == true
      true
-  elsif array.none? {|element| element.length < 4} = true
+  elsif array.none? {|element| element.length < 4} == true
     false
   end
 end
