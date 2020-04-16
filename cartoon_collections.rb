3d2ae8d261f741d827ac44f31c9d9979a1e44e70
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-16 22:42:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.include? {|element| element.length >= 4 }
+  array.any? {|element| element.length >= 4 }
 end
 
 def find_the_cheese# code an argument here
