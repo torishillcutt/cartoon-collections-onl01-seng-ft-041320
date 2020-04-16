52721202bc6a31f9ca042e9087e663f755b88461
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-16 23:26:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.detect{|element| element.is_a?("cheddar", "gouda", "camembert")}
+  array.detect{|element| element.is_a?(cheese_types)}
 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
